58de0d18e84e68c8f2775c668fa0cf7eef614e77
Hayley Witherell <hayleywitherell@gmail.com>
2021-08-25 14:43:57 -0600

    Add movieData.js

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ class App extends Component {
   }
 
   render() {
-  return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1>test</h1>
+      </div>
+    );
   }
-}  
+} 
 
 export default App;
