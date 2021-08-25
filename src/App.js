9cc09b1b0308b2bbca0cf868f048ddb0be3f6e9f
Hayley Witherell <hayleywitherell@gmail.com>
2021-08-25 14:57:38 -0600

    Add Poster files

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1>test</h1>
-      </div>
+      <main className="App">
+        <h1>Rancid Tomatillos</h1>
+        <Posters posters={this.state.movies} />
+      </main>
     );
   }
 } 
