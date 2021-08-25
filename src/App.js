57480f54e213ebb0dc1d750b0be2432570558fe7
Hayley Witherell <hayleywitherell@gmail.com>
2021-08-25 14:42:30 -0600

    Add constructor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+
+  render() {
   return (
     <div className="App">
       <h1>test</h1>
     </div>
   );
-}
+  }
+}  
 
 export default App;
