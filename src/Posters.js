94f9f3b3efc2f3034600dcbab3574fe814bc178a
William Phelps <williamphelps13@gmail.com>
2021-08-25 15:51:57 -0600

    Add functionality to display posters

--- src/Posters.js
+++ src/Posters.js
@@ -2,13 +2,9 @@ import React from 'react'
 
 const Posters = ({ posters }) => {
   const moviePosters = posters.map(poster => {
-    return (
-      <Poster
-        url={poster.poster_path}
-      />
-    )
+    return (<img className='poster-icon' src={poster.poster_path}></img>)
   })
-
+  
   return (
     <section className='posters-container'>
       {moviePosters}
