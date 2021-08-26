ee82ddd93e8be539a0375b666a00b8c36bbb33d2
Hayley Witherell <hayleywitherell@gmail.com>
2021-08-25 20:24:50 -0600

    Display movie card

--- src/Movie.js
+++ src/Movie.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 import './Movie.css';
 
 const Movie = ({ movie }) => {
+
   return (
-    <h1>this is working</h1>
+    <section className='movie-card'>
+      <h2>{movie.title}</h2>
+      <p>★ {movie.average_rating}</p>
+      <p>{movie.tagline}</p>
+      <p>{movie.overview}</p>
+      <p>{movie.genres}</p>
+      <p>{movie.runtime}</p>
+      <p>{movie.budget}</p>
+      <p>{movie.release_date}</p>
+      <p>{movie.revenue}</p>
+    </section>
   )
 }
 
