e83b92a22f2ecb073f5b1e0ec2f1b1a4f599aaf9
Hayley Witherell <hayleywitherell@gmail.com>
2021-08-30 17:02:50 -0600

    Confirm correct posters display

--- cypress/integration/posters_spec.js
+++ cypress/integration/posters_spec.js
@@ -9,7 +9,28 @@ describe('Rancid Tomatillos Posters Page', () => {
 
   // dummy test to make sure things are hooked up correctly
 
-  it('Should confirm that true is equal to true', () => {
-    expect(true).to.equal(true)
+  // it('Should confirm that true is equal to true', () => {
+  //   expect(true).to.equal(true)
+  // });
+
+  it('Should confirm that user can visit localhost:3000', () => {
+    cy.get('h1')
+    .contains('Rancid Tomatillos')
   });
+
+  it('Should confirm that posters display', () => {
+    cy.get('input[type="image"]') 
+    .should('be.visible')
+  });
+
+  it('Should confirm that the correct posters display', () => {
+    cy.get('input')
+    .should('have.attr', 'src')
+    .should('include','https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg')
+  });
+
+  it('Should confirm that all posters display', () => {
+    cy.get('input[type="image"]') 
+  });
+
 });
