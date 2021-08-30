0bc6bf6d292058624b7e8022932019f91f2ec408
Hayley Witherell <hayleywitherell@gmail.com>
2021-08-30 17:55:54 -0600

    Run first successful test test

--- cypress/integration/movie_spec.js
+++ cypress/integration/movie_spec.js
@@ -1,3 +1,23 @@
+describe('Rancid Tomatillos Movie Page', () => {
+
+  // localhost url
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+
+  // test singleMovie api?
+
+  // dummy test to make sure things are hooked up correctly
+
+//   it('Should confirm that true is equal to true', () => {
+//     expect(true).to.equal(true)
+//   });
+// });
+
+
+  
 // see posters hide
 // see movie info loads
 
