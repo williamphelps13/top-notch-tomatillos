81f5b7a3daf62e19c1cd0382da4c0f9ad75e832e
Hayley <78039850+hayleyw7@users.noreply.github.com>
2021-08-30 17:50:37 -0600

    Revert "Revert "Test/posters""

---
+++ cypress/integration/posters_spec.js
@@ -0,0 +1,47 @@
+
+describe('Rancid Tomatillos Posters Page', () => {
+
+  // localhost url
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  // test movies api?
+
+  // dummy test to make sure things are hooked up correctly
+
+  // it('Should confirm that true is equal to true', () => {
+  //   expect(true).to.equal(true)
+  // });
+
+  // real tests
+
+  it('Should confirm that user can visit localhost:3000', () => {
+    cy.get('h1')
+    .contains('Rancid Tomatillos')
+  });
+
+  it('Should confirm that posters display', () => {
+    cy.get('input[type="image"]') 
+    .should('be.visible')
+  });
+
+  it('Should confirm that the correct posters display', () => {
+    cy.get('input[type="image"]')
+    .should('have.attr', 'src')
+    .should('include','https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg')
+  });
+
+  it('Should confirm that all posters display', () => {
+    cy.get('input[type="image"]')
+    .its('length')
+    .should('eq', 40)
+  });
+
+  it('Should be able to click poster directing user the corresponding movie page', () => {
+    cy.get('input[alt="Mulan"]')
+    .click()
+    .get('h2').should('contain', 'Mulan')
+  });
+});
