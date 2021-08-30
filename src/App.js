a3ab26c02efb94b4f711e4d558a4df94b71f687d
William Phelps <williamphelps13@gmail.com>
2021-08-30 15:43:23 -0600

    Refactor Movies Fetch call since in API file

--- src/App.js
+++ src/App.js
@@ -14,10 +14,9 @@ class App extends Component {
   }
   
   componentDidMount() {
-    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-      .then(response => response.json())
-      .then(data => {this.setState({movies: data.movies})})
-      .catch(error => this.setState({error: error}))
+    fetchMovies()
+    .then(data => this.setState({movies: data.movies}))
+    .catch(error => this.setState({error: error}))
   }
   
   clickPoster = (id) => {   
