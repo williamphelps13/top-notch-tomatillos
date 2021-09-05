a135b0a317911701d8773d388f49f7d5833f8d36
William Phelps <williamphelps13@gmail.com>
2021-09-04 22:36:01 -0600

    Add single movie data cleaning function incorporating dayjs etc

--- src/data-cleaning.js
+++ src/data-cleaning.js
@@ -9,3 +9,18 @@ export const cleanPosterData = (data) => {
     }
   })
 }
+
+export const cleanMovieData = (data) => {
+  return data = {
+    movie: {
+      backdrop_path: data.movie.backdrop_path,
+      title: data.movie.title,    
+      average_rating: `★ ${data.movie.average_rating.toFixed(1)}`,
+      tagline: data.movie.tagline,
+      overview: data.movie.overview,
+      release_date: dayjs(data.movie.release_date).format('YYYY'),
+      runtime: data.movie.runtime ? `${data.movie.runtime} minutes` : '',
+      genres: data.movie.genres
+    }
+  }
+}
