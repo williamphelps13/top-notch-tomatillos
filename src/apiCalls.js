a65768f3c2d6a63d6e496e0846f2be3b4e45b111
William Phelps <williamphelps13@gmail.com>
2021-09-04 22:33:20 -0600

    Create conditional with different messages for each error

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,14 @@
 export const getData = (url) => {
   return fetch(url)
     .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else if (response.status === 404) {
+        throw Error('404 Error aka "We looked but did not find anything in that spot"')
+      } else if (response.status === 500) {
+        throw Error('500 Error aka "It\s not you it\s me -  try again soon!"')
+      } else {
+      throw Error('Other Error - Something miscellaneous went wrong')
+      }
     })
 }
