2e9b050f9a9d01aedca2ea6afcea0b31988ab6b2
William Phelps <williamphelps13@gmail.com>
2021-09-04 22:32:46 -0600

    Refactor to create dynamic API fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,5 @@
-export function fetchMovies() {
-  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies').then(response => response.json())
-}
-
-export function fetchSingleMovie(id) {
-  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`).then(response => response.json())
+export const getData = (url) => {
+  return fetch(url)
+    .then(response => {
+    })
 }
