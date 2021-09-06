ebf645e4ca265d08fe8af98b0f0c2fb5dbb984a8
William Phelps <williamphelps13@gmail.com>
2021-09-06 14:01:43 -0600

    Add 500 error movie page test

--- cypress/integration/api_calls_spec.js
+++ cypress/integration/api_calls_spec.js
@@ -5,3 +5,11 @@ describe('API Calls Error Messages', () => {
     cy.get('p[class="error-message"]')
       .contains('500 error')
   })
+
+  it('Should be able to display 500 error on movie page', () => {
+    cy.goToMovie500Page();
+    cy.get('p[class="error-message"]')
+      .contains('500 error')
+  })
+
+ 
