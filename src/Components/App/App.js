ab3bdf02edfbb89f5234d8388c15f887a2a8b369
William Phelps <williamphelps13@gmail.com>
2021-09-05 19:50:34 -0600

    Make app components folder

---
+++ src/Components/App/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import './App.css';
+import Error from '../Error/Error';
+import Loader from '../Loader/Loader';
+import Posters from '../Posters/Posters';
+import Movie from '../Movie/Movie';
+import { getData } from '../../utilities/apiCalls';
+import { cleanPosterData } from '../../utilities/dataCleaning';
+import { Route } from 'react-router-dom';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      error: ''
+    }
+  }
+  
+  componentDidMount() {
+    getData('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    .then(data => cleanPosterData(data))
+    .then(data => this.setState({movies: data}))
+    .catch(error => this.setState({error: error.message}))
+  }
+
+  render() {
+    const {movies, error} = this.state;
+   
+    return (
+      <main className="App">
+        <h1 className="App-header">Rancid Tomatillos</h1>
+        
+        <Route exact path='/' 
+          render={() => {
+        
+          if (error) {
+
+            return < Error message={error} /> 
+
+          } else if (!movies.length) {
+
+            return <Loader item='movie posters are' />
+
+          } else {
+
+            return <Posters posters={movies}/>
+          } 
+        }}
+      />
+
+        <Route 
+          exact path='/:id' 
+          render={({ match }) => {
+            const clickedMovieID = parseInt(match.params.id);
+            return <Movie movieID={clickedMovieID} /> 
+          }}
+        />
+        
+      </main>
+    );
+  }
+} 
+
+export default App;
