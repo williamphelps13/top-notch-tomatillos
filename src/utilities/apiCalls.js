99d0850958fe12ae90e100e3f66f24b05f50174e
William Phelps <williamphelps13@gmail.com>
2021-09-05 19:50:47 -0600

    Fix warning with escapes

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -6,7 +6,7 @@ export const getData = (url) => {
       } else if (response.status === 404) {
         throw Error('404 Error aka "We looked but did not find anything in that spot"')
       } else if (response.status === 500) {
-        throw Error('500 Error aka "It\s not you it\s me -  try again soon!"')
+        throw Error('500 Error aka "It\'s not you it\'s me -  try again soon!"')
       } else {
       throw Error('Other Error - Something miscellaneous went wrong')
       }
