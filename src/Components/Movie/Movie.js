205715dbc3057fb3c5a7efa1267e98f891795c57
William Phelps <williamphelps13@gmail.com>
2021-10-01 12:15:01 -0600

    Refactor to hooks

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -1,29 +1,36 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect} from 'react'
 import './Movie.css'
 import Error from '../Error/Error'
 import Loader from '../Loader/Loader'
 import { getMovieData } from '../../utilities/apiCalls'
 import { Link } from 'react-router-dom'
 
-class Movie extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      singleMovie: {},
-      error: ''
-    }
+const Movie = ({ movieID }) => {
+  const [singleMovie, setSingleMovie] = useState({})
+  const [error, setError] = useState('')
+
+  const {backdrop_path, title, average_rating, tagline, overview, release_date, runtime, genres} = singleMovie
+
+  useEffect(() => {
+    getMovieData(movieID)
+    .then(data => setSingleMovie(data))
+    .catch(error => setError(error.message))
+  })
+
+  conditionalPostersDisplay = () => {
+    const {singleMovie, error} = this.state
+
+    return error ? <Error message={error} page='movie information' /> 
+      : !Object.keys(singleMovie).length ? <Loader item='movie information is' /> 
+      : this.displayMovieCard()
   }
 
-  componentDidMount() {
-    getMovieData(this.props.movieID)
-    .then(data => this.setState({singleMovie: data}))
-    .catch(error => this.setState({error: error.message}))
-  }
+  return (
+    error ? <Error message={error} page='movie information' /> 
 
-  displayMovieCard = () => {
-    const {backdrop_path, title, average_rating, tagline, overview, release_date, runtime, genres} = this.state.singleMovie
+    : !Object.keys(singleMovie).length ? <Loader item='movie information is' /> 
 
-    return (
+    :
       <section className='movie-background' style={{ backgroundImage: `url(${backdrop_path})` }}>
         <section className='movie-container'>
           <section className='movie-card'>
@@ -42,20 +49,7 @@ class Movie extends Component {
           </Link> 
         </section>
       </section>
-    )
-  }
-
-  conditionalPostersDisplay = () => {
-    const {singleMovie, error} = this.state
-
-    return error ? <Error message={error} page='movie information' /> 
-      : !Object.keys(singleMovie).length ? <Loader item='movie information is' /> 
-      : this.displayMovieCard()
-  }
-
-  render = () => {
-    return this.conditionalPostersDisplay()
-  }
+  )
 }
 
 export default Movie
