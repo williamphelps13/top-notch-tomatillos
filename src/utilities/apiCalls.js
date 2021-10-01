77fe64ab3f19c39a7c50db24abce560482b1c6c6
William Phelps <williamphelps13@gmail.com>
2021-10-01 11:45:54 -0600

    Make checkForError a seperate function

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -1,14 +1,25 @@
-export const getData = (url) => {
-  return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json()
-      } else if (response.status === 404) {
-        throw Error('404 error ("We looked but did not find anything in that spot") - Head back, and try again.')
-      } else if (response.status === 500) {
-        throw Error('500 error ("It\'s not you; it\'s me") -  Please try again soon.')
-      } else {
-      throw Error('Other error - Something miscellaneous went wrong.')
-      }
-    })
+import { cleanPosterData, cleanMovieData } from '../../utilities/dataCleaning';
+
+export const getPosterData = () => {
+  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    .then(response => checkForError(response))
+    .then(data => cleanPosterData(data))
+}
+
+export const getMovieData = (movieID) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${movieID}`)
+    .then(response => checkForError(response))
+    .then(data => cleanMovieData(data))
+}
+
+const checkForError = (response) => {
+  if (response.ok) {
+    return response.json()
+  } else if (response.status === 404) {
+    throw Error('404 error ("We looked but did not find anything in that spot") - Head back, and try again.')
+  } else if (response.status === 500) {
+    throw Error('500 error ("It\'s not you; it\'s me") -  Please try again soon.')
+  } else {
+  throw Error('Other error - Something miscellaneous went wrong.')
+  }
 }
