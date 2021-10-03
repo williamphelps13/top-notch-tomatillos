6d0668d66aa2ca0a59184d448ec585c07ebb028f
William Phelps <williamphelps13@gmail.com>
2021-10-03 11:02:25 -0600

    Add get Video and get Similar to Movies component

--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -2,35 +2,49 @@ import React, {useState, useEffect} from 'react'
 import './Movie.css'
 import Error from '../Error/Error'
 import Loader from '../Loader/Loader'
-import { getMovieData } from '../../utilities/apiCalls'
+import Video from '../Video/Video'
+import { getMovieData, getVideo, getSimilar } from '../../utilities/apiCalls'
 import { Link } from 'react-router-dom'
 
 const Movie = ({ movieID }) => {
   const [singleMovie, setSingleMovie] = useState({})
+  const [videoKey, setVideoKey] = useState('')
+  const [similar, setSimilar] = useState('')
   const [error, setError] = useState('')
-
-  const {backdrop_path, title, average_rating, tagline, overview, release_date, runtime, genres} = singleMovie
+  
+  const {backdropPath, title, rating, tagline, overview, releaseDate, runtime, genres} = singleMovie
 
   useEffect(() => {
     getMovieData(movieID)
-    .then(data => setSingleMovie(data))
-    .catch(error => setError(error.message))
-  })
-  
+      .then(data => setSingleMovie(data))
+      .catch(error => setError(error.message))
+
+    getVideo(movieID)
+      .then(data => {
+        console.log('video', data)
+        setVideoKey(data)
+      })
+      .catch(error => setError(error.message))
+
+    getSimilar(movieID)
+      .then(data => setSimilar(data))
+      .catch(error => setError(error.message)) 
+  }, [])
+
   return (
     error ? <Error message={error} page='movie information' /> 
 
     : !Object.keys(singleMovie).length ? <Loader item='movie information is' /> 
 
     :
-      <section className='movie-background' style={{ backgroundImage: `url(${backdrop_path})` }}>
+      <section className='movie-background' style={{ backgroundImage: `url(${backdropPath})` }}>
         <section className='movie-container'>
           <section className='movie-card'>
             <h2 className='title'>{title}</h2>
-            <p className='rating'>{average_rating}</p>
+            <p className='rating'>{rating}</p>
             <p className='tagline'>{tagline}</p>
             <p className='overview'>{overview}</p>
-            <p className='release'>{release_date}</p>
+            <p className='release'>{releaseDate}</p>
             <p className='runtime'>{runtime}</p>
             <div className='genre-container'>
               {genres.map(genre => <p className='genres' key={genre}>{genre}</p>)}
@@ -40,6 +54,9 @@ const Movie = ({ movieID }) => {
             <button className='back-btn'>BACK</button>
           </Link> 
         </section>
+        <div>
+          <Video id={videoKey} title={title} />
+        </div>
       </section>
   )
 }
