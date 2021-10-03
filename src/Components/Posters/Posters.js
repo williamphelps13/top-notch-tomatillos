029c6fefd607e78cd7990b3b3860bd29e2db8fec
William Phelps <williamphelps13@gmail.com>
2021-10-03 11:03:11 -0600

    Add load page buttons

--- src/Components/Posters/Posters.js
+++ src/Components/Posters/Posters.js
@@ -2,28 +2,41 @@ import React from 'react'
 import './Posters.css'
 import { Link } from 'react-router-dom'
 
-const Posters = ({ posters }) => {
+const Posters = ({ posters, loadPage }) => {
 
   const moviePosters = posters.map(poster => {
-    const {id, poster_path, title} = poster
+    const {id, populartity, posterPath, title, rating, votes, releaseDate} = poster
 
     return (
       <Link
         to={`/${id}`}
         key={id}  
       >
-      <img
-        src={poster_path}
-        className='poster-icon'
-        alt={`${title} Movie Poster and Button`}
-        id={id}
-      />
+      {/* <article className='flip-card-container'> */}
+        <img
+          src={posterPath}
+          className='poster-icon'
+          alt={`${title} Movie Poster and Button`}
+          id={id}
+        />
+        {/* <div className='flip-card-back'>
+          <p>{rating}</p>
+          <p>{releaseDate}</p>
+        </div> */}
+      {/* </article> */}
       </Link>
     )
   })
   
   return (
     <section className='posters-container'>
+      <section>
+        <button onClick={event => loadPage(1)}>1</button>
+        <button onClick={event => loadPage(2)}>2</button>
+        <button onClick={event => loadPage(3)}>3</button>
+        <button onClick={event => loadPage(4)}>4</button>
+        <button onClick={event => loadPage(5)}>5</button>
+      </section>
       {moviePosters}
     </section>
   )
