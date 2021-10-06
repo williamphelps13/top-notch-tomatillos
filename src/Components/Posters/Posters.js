e02f0cf232477513dc4ba46a81bd8528c8f6030c
William Phelps <williamphelps13@gmail.com>
2021-10-05 18:11:42 -0600

    Add mini poster conditional className

--- src/Components/Posters/Posters.js
+++ src/Components/Posters/Posters.js
@@ -2,41 +2,25 @@ import React from 'react'
 import './Posters.css'
 import { Link } from 'react-router-dom'
 
-const Posters = ({ posters, loadPage }) => {
-
-  const moviePosters = posters.map(poster => {
-    const {id, populartity, posterPath, title, rating, votes, releaseDate} = poster
-
+const Posters = ({ pageOfMovies, size }) => {
+  const moviePosters = pageOfMovies.map(poster => {
     return (
       <Link
-        to={`/${id}`}
-        key={id}  
+        to={`/movie/${poster.id}`}
+        key={poster.id}  
       >
-      {/* <article className='flip-card-container'> */}
         <img
-          src={posterPath}
-          className='poster-icon'
-          alt={`${title} Movie Poster and Button`}
-          id={id}
+          src={poster.posterPath}
+          className={`${size}-poster-icon`}
+          alt={`${poster.title} Movie Poster and Button`}
+          id={poster.id}
         />
-        {/* <div className='flip-card-back'>
-          <p>{rating}</p>
-          <p>{releaseDate}</p>
-        </div> */}
-      {/* </article> */}
       </Link>
     )
   })
-  
+
   return (
-    <section className='posters-container'>
-      <section>
-        <button onClick={event => loadPage(1)}>1</button>
-        <button onClick={event => loadPage(2)}>2</button>
-        <button onClick={event => loadPage(3)}>3</button>
-        <button onClick={event => loadPage(4)}>4</button>
-        <button onClick={event => loadPage(5)}>5</button>
-      </section>
+    <section>
       {moviePosters}
     </section>
   )
