a169843ed653b233f3e5c437556224ec53c66747
William Phelps <williamphelps13@gmail.com>
2021-10-05 18:12:20 -0600

    Make dynamic API call

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -1,25 +1,39 @@
-import { cleanPosterData, cleanMovieData, cleanVideoData } from './dataCleaning'
+import { cleanPosterData, cleanMovieData, cleanVideoData, cleanImageData } from './dataCleaning'
 import dayjs from 'dayjs'
 
-const now = dayjs().format('YYYY-MM-DD')
-const oneMonthAgo = dayjs().subtract(1, 'month').format('YYYY-MM-DD')
+export const getPosterData = (start, pageNum, sortBy, genre) => {
+  const now = dayjs().format('YYYY-MM-DD')
 
-export const getPosterData = (pageNum) => {
-  return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=61beee5760e383289f2184ab4e7ff153&with_original_language=en&sort_by=popularity.desc&include_adult=false&page=${pageNum}&primary_release_date.gte=${oneMonthAgo}&primary_release_date.lte=${now}&with_watch_monetization_types=flatrate`)
+  if (start === 'oneMonthAgo') {
+    start = dayjs().subtract(1, 'month').format('YYYY-MM-DD')
+  } else if (start === 'oneYearAgo') { 
+    start = dayjs().subtract(1, 'year').format('YYYY-MM-DD')
+  }
+
+  if (genre === 'all') {
+    return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=61beee5760e383289f2184ab4e7ff153&with_original_language=en&include_adult=false&with_watch_monetization_types=flatrate&primary_release_date.gte=${start}&primary_release_date.lte=${now}&page=${pageNum}&sort_by=${sortBy}`)
     .then(response => checkForError(response))
-    .then(data => {
-      console.log('discovery', data) 
-      return cleanPosterData(data)
-    })
+    .then(data => cleanPosterData(data))
+  } else {
+    const genreNum = parseInt(genre)
+
+    return fetch(`https://api.themoviedb.org/3/discover/movie?api_key=61beee5760e383289f2184ab4e7ff153&with_original_language=en&include_adult=false&with_watch_monetization_types=flatrate&primary_release_date.gte=${start}&primary_release_date.lte=${now}&page=${pageNum}&sort_by=${sortBy}&with_genres=${genreNum}`)
+      .then(response => checkForError(response))
+      .then(data => cleanPosterData(data))
+  }
+}
+
+// &primary_release_year=2021
+export const getSearchResults = (pageNum, search) => {
+  return fetch(`https://api.themoviedb.org/3/search/movie?api_key=61beee5760e383289f2184ab4e7ff153&include_adult=false&page=${pageNum}&query=${search}`)
+    .then(response => checkForError(response))
+    .then(data => cleanPosterData(data))
 }
 
 export const getMovieData = (movieID) => {
   return fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=61beee5760e383289f2184ab4e7ff153`)
     .then(response => checkForError(response))
-    .then(data => {
-      console.log('movie', data) 
-      return cleanMovieData(data)
-    })
+    .then(data => cleanMovieData(data))
 }
 
 export const getVideo = (movieID) => {
@@ -31,9 +45,14 @@ export const getVideo = (movieID) => {
 export const getSimilar = (movieID) => {
   return fetch(`https://api.themoviedb.org/3/movie/${movieID}/similar?api_key=61beee5760e383289f2184ab4e7ff153`)
     .then(response => checkForError(response))
-    .then(data => {
-      console.log('similar', data) 
-    })
+    .then(data => cleanPosterData(data))
+    .then(data => data.sort((a, b) => b.popularity - a.popularity))
+}
+
+export const getImages = (movieID) => {
+  return fetch(`https://api.themoviedb.org/3/movie/${movieID}/images?api_key=61beee5760e383289f2184ab4e7ff153`)
+    .then(response => checkForError(response))
+    .then(data => cleanImageData(data))
 }
 
 const checkForError = (response) => {
