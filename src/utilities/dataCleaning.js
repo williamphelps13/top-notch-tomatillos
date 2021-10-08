a93a454368b453b65932489c857cb0676bdcba3d
William Phelps <williamphelps13@gmail.com>
2021-10-08 09:45:19 -0600

    Add back popularity for sorting of similar movies

--- src/utilities/dataCleaning.js
+++ src/utilities/dataCleaning.js
@@ -4,11 +4,12 @@ export const cleanPosterData = (data) => {
   return data.results.map(movie => {
     return {
       id: movie.id,
-      posterPath: movie.poster_path === null ? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1mcHVzLjjPjJNNYOT8v2f0rYU2C5wzvf_BnvhayR8N6ENCTXSP9quG0ejpmJ2w6EBWYw&usqp=CAU' : `https://image.tmdb.org/t/p/w500${movie.poster_path}` , 
+      posterPath: movie.poster_path === null ? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1mcHVzLjjPjJNNYOT8v2f0rYU2C5wzvf_BnvhayR8N6ENCTXSP9quG0ejpmJ2w6EBWYw&usqp=CAU' : `https://image.tmdb.org/t/p/w500${movie.poster_path}` ,
       title: movie.title,
       rating: movie.rating ? `★ ${movie.vote_average}`: '',
       votes: movie.votes ? movie.vote_count: '',
-      releaseDate: movie.releaseDate ? dayjs(data.release_date).format('MM-DD-YYYY'): ''
+      releaseDate: movie.releaseDate ? dayjs(data.release_date).format('MM-DD-YYYY'): '',
+      popularity: movie.popularity
     }
   })
 }
